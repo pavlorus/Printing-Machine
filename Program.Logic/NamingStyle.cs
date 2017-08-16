260d01a4a1e28773e5f554fe3c407d17f9468e3d
pavlo <pavlo@DESKTOP-RL1NFHP>
2017-08-16 21:44:51 +0300

    I think I dont know where to use NamingStile class.

--- Program.Logic/NamingStyle.cs
+++ Program.Logic/NamingStyle.cs
@@ -3,7 +3,7 @@ using Utilities;
 namespace Program.Logic
 {
     class NamingStyle
-    {
+    {   // I think I need to delete this class, becouse I don't know where use this solution.
         public string SheetId { get; set; }
 
         public string SheetName { get; set; }
